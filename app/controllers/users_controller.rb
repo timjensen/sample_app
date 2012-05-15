2ff534749b8b650df41f646dc9dce910455212e4
Tim Jensen <timdavidjensen@gmail.com>
2012-05-15 15:58:24 +1200

    finished chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
   
   def show
     @user = User.find(params[:id])
@@ -28,6 +29,12 @@ class UsersController < ApplicationController
   def edit
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+  
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
@@ -50,5 +57,9 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
