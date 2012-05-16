32a0e6d3db5a5cd09b077362f829d8080d9a78e9
Tim Jensen <timdavidjensen@gmail.com>
2012-05-16 13:43:22 +1200

    finished chapter 10

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -55,7 +55,23 @@ describe "Authentication" do
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
+        
+        describe "in the Microposts controller" do
 
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before do
+            micropost = FactoryGirl.create(:micropost)
+            delete micropost_path(micropost)
+          end
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+      
         describe "after signing in" do
 
           it "should render the desired protected page" do
